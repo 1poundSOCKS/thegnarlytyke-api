b23ddac1c4bb42fed2332c2e9fc467c1259ab517
1poundSOCKS <mathew.coburn@gmail.com>
2022-08-11 12:22:19 +0100

    Fix to deployment

--- lambda/scripts/deploy_to_test.py
+++ lambda/scripts/deploy_to_test.py
@@ -2,6 +2,7 @@ import sys
 from lambda_helpers import *
 
 def DeployToTest(function_name, function_source_dir):
+  print(f'Updating function "{function_name}" with "{function_source_dir}"')
   updated_function_def = UpdateLambdaFunctionCode(function_name, function_source_dir, "../build")
   version = updated_function_def["Version"]
   print(f"Successfully deployed function '{function_name}' (version={version})")
@@ -11,9 +12,10 @@ try:
   functions = ['gnarly-logon','gnarly-authenticate-user','gnarly-load-data','gnarly-save-data','gnarly-save-image']
 
   for function_name in functions:
-    source_folder = function_name.replace('-','_')
+    source_folder = function_name
+    source_folder = source_folder.replace('-','_')
     source_path = f'../functions/{source_folder}/source'
-    DeployToTest(function_name,source_folder)
+    DeployToTest(function_name,source_path)
 
 except BaseException as err:
   print(f"Failed (error={err})")
