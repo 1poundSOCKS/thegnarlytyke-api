7e2dea721a1ee9e54b848fb43484beb73969f721
1poundSOCKS <mathew.coburn@gmail.com>
2022-08-11 12:27:21 +0100

    Deploy script update

--- lambda/scripts/deploy_to_dev.py
+++ lambda/scripts/deploy_to_dev.py
@@ -2,15 +2,19 @@ import sys
 from lambda_helpers import *
 
 def DeployToDev(function_name, function_source_dir):
+  print(f'Updating function "{function_name}" with "{function_source_dir}"')
   updated_function_def = UpdateLambdaFunctionCode(function_name, function_source_dir, "../build")
   version = updated_function_def["Version"]
   print(f"Successfully deployed function '{function_name}' (version={version})")
 
 try:
-  DeployToDev("gnarly-authenticate-user","../functions/gnarly_authenticate_user/source")
-  DeployToDev("gnarly-load-data","../functions/gnarly_load_data/source")
-  DeployToDev("gnarly-save-data","../functions/gnarly_save_data/source")
-  DeployToDev("gnarly-save-image","../functions/gnarly_save_image/source")
+  functions = ['gnarly-logon','gnarly-authenticate-user','gnarly-load-data','gnarly-save-data','gnarly-save-image']
+
+  for function_name in functions:
+    source_folder = function_name
+    source_folder = source_folder.replace('-','_')
+    source_path = f'../functions/{source_folder}/source'
+    DeployToDev(function_name,source_path)
 
 except BaseException as err:
   print(f"Failed (error={err})")
